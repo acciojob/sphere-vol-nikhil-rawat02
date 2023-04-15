b21528479272e106069c95404028d9a7947d0a94
nikhil-rawat02 <117645790+nikhil-rawat02@users.noreply.github.com>
2023-04-15 19:22:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
+let volume = document.getElementById("volume");
+let radius = document.getElementById("radius");
 function volume_sphere() {
     //Write your code here
-  
-} 
+	let radiusVal = parseInt(radius.value);
 
+	let vl = radiusVal * radiusVal * radiusVal  * 3.14 * 4 / 3;
+	volume.value = vl.toString();
+}
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
